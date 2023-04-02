fa7bcfd2c9c2e694b9851a9348d683e269b39f63
Joseph-Ano <80886311+Joseph-Ano@users.noreply.github.com>
2023-04-02 15:50:07 +0800

    added error checking

--- app.py
+++ app.py
@@ -13,12 +13,7 @@ def main():
     mm = st.sidebar.text_input("Main memory size", key="mm")
     cache_memory_size = st.sidebar.text_input("Cache memory size", key="cache_size")
     program_flow = st.sidebar.text_input("Program flow", key="program_flow")
-
-    try:
-        set_size = int(set_size)
-        block_size = int(block_size)
-    except:
-        pass
+    existError = False
     
     # set_size = 2 #blocks
     # block_size = 2 #words
@@ -26,82 +21,105 @@ def main():
     # cache_memory_size = "4 blocks"
     # program_flow = "blocks 1 7 5 0 2 1 5 6 5 2 2 0"
 
-    cache_check = 1
-    cache_access_time = 1
-    memory_access_time = 10
+    simulate = st.button("Simulate", key="simulate")
 
-    if(st.button("Simulate", key="simulate")):
-        snapshot, hit, miss, num_sets = main_algo(set_size, block_size, mm, cache_memory_size, program_flow)
+    if(simulate):
+        temp = cache_memory_size.split(" ")
+        if(len(temp) != 2 or (temp[1] != "blocks" and temp[1] != "words")):
+            st.warning("Cache memory size input is wrong format") 
+            existError = True
 
-        if(snapshot == -5):
-            st.warning("Cache memory size is not a valid input given the Set Size and/or Block size")
-            
-        else:
-            miss_penalty = cache_check + (block_size*memory_access_time) + cache_access_time
+        temp = program_flow.split(" ")
+        if(temp[0] != "blocks" or temp[0] != "words" and len(temp) < 2):
+            st.warning("Program flow input is wrong format") 
+            existError = True
 
-            hit_rate = hit/(hit+miss)
-            miss_rate = miss/(hit+miss)
-            avg_access_time = hit_rate*cache_access_time + miss_penalty*miss_rate
-            total_access_time = (hit*block_size*cache_access_time) + (miss*block_size*(cache_access_time+memory_access_time)) + cache_check*miss
+        try:
+            int(cache_memory_size[0])
+        except ValueError:
+            existError = True
+            st.warning("Cache memory size must be an integer")
 
+        try:
+            set_size = int(set_size)
+            block_size = int(block_size)
+        except ValueError:
+            existError = True
+            st.warning("Set size and Block size must be an integer")
 
-            st.write("Cache hits: {cache_hit}".format(cache_hit=hit))
-            st.write("Cache miss: {cache_miss}".format(cache_miss=miss))
-            st.write("Miss penalty: {penalty}".format(penalty=miss_penalty))
-            st.write("Average memory access time: {access_time}".format(access_time=avg_access_time))
-            st.write("Total memory access time: {access_time}".format(access_time=total_access_time))
+        if(not existError):
+            cache_check = 1
+            cache_access_time = 1
+            memory_access_time = 10
+            snapshot, hit, miss, num_sets = main_algo(set_size, block_size, mm, cache_memory_size, program_flow)
 
-            table_header = ["Sets"]
+            if(snapshot == -5):
+                st.warning("Cache memory size is not a valid input given the Set Size and/or Block size")
 
-            for row in range(0, len(snapshot), 1):
-                blocks_in_set = len(snapshot[row])
+            else:
+                miss_penalty = cache_check + (block_size*memory_access_time) + cache_access_time
 
-                while(blocks_in_set < set_size):
-                    snapshot[row].append([])
-                    blocks_in_set+=1
-
-                for col in range(0, set_size, 1):
-                    if not snapshot[row][col]:
-                        snapshot[row][col] = "empty"
-                    else:
-                        snapshot[row][col] = snapshot[row][col][0]
-
-            for i in range(0, set_size, 1):
-                table_header.append("Block {i}".format(i=i))
-
-            for i in range(0, len(snapshot),1):
-                snapshot[i].insert(0, "Set {i}".format(i=i))
-
-            np_snapshot = np.array(snapshot)
-
-            print(snapshot)
-
-            df = pd.DataFrame(np_snapshot, columns = table_header)
-            st.table(df)
+                hit_rate = hit/(hit+miss)
+                miss_rate = miss/(hit+miss)
+                avg_access_time = hit_rate*cache_access_time + miss_penalty*miss_rate
+                total_access_time = (hit*block_size*cache_access_time) + (miss*block_size*(cache_access_time+memory_access_time)) + cache_check*miss
 
 
-            table_header = [table_header]
-            table_header.extend(snapshot)
+                st.write("Cache hits: {cache_hit}".format(cache_hit=hit))
+                st.write("Cache miss: {cache_miss}".format(cache_miss=miss))
+                st.write("Miss penalty: {penalty}".format(penalty=miss_penalty))
+                st.write("Average memory access time: {access_time}".format(access_time=avg_access_time))
+                st.write("Total memory access time: {access_time}".format(access_time=total_access_time))
 
-            t = Texttable()
-            t.add_rows(table_header)
+                table_header = ["Sets"]
 
-            formatted_table = t.draw()
+                for row in range(0, len(snapshot), 1):
+                    blocks_in_set = len(snapshot[row])
 
-            text_contents = '''
-    Cache hits: {cache_hit}\n
-    Cache miss: {cache_miss}\n
-    Average memory access time: {avg_access_time}\n
-    Total memory access time: {total_access_time}\n
-    {snapshot}
-            '''.format(cache_hit=hit, cache_miss=miss, penalty=miss_penalty, avg_access_time=avg_access_time, 
-                    total_access_time=total_access_time, snapshot=formatted_table)
+                    while(blocks_in_set < set_size):
+                        snapshot[row].append([])
+                        blocks_in_set+=1
 
-            st.download_button(
-                label="Download",
-                data=text_contents,
-                file_name='Group5.txt',
-            )   
+                    for col in range(0, set_size, 1):
+                        if not snapshot[row][col]:
+                            snapshot[row][col] = "empty"
+                        else:
+                            snapshot[row][col] = snapshot[row][col][0]
+
+                for i in range(0, set_size, 1):
+                    table_header.append("Block {i}".format(i=i))
+
+                for i in range(0, len(snapshot),1):
+                    snapshot[i].insert(0, "Set {i}".format(i=i))
+
+                np_snapshot = np.array(snapshot)
+
+                df = pd.DataFrame(np_snapshot, columns = table_header)
+                st.table(df)
+
+
+                table_header = [table_header]
+                table_header.extend(snapshot)
+
+                t = Texttable()
+                t.add_rows(table_header)
+
+                formatted_table = t.draw()
+
+                text_contents = '''
+        Cache hits: {cache_hit}\n
+        Cache miss: {cache_miss}\n
+        Average memory access time: {avg_access_time}\n
+        Total memory access time: {total_access_time}\n
+        {snapshot}
+                '''.format(cache_hit=hit, cache_miss=miss, penalty=miss_penalty, avg_access_time=avg_access_time, 
+                        total_access_time=total_access_time, snapshot=formatted_table)
+
+                st.download_button(
+                    label="Download",
+                    data=text_contents,
+                    file_name='Group5.txt',
+                )   
 
 if __name__ == "__main__":
     main()
